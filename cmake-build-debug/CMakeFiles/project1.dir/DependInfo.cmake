440710e85b4dc37e54f93a237a8d51a2fc33d1a4
Quincy Wilcox <qgwilcox@gmail.com>
2021-09-16 16:13:34 -0600

    Include the pass off cases

---
+++ cmake-build-debug/CMakeFiles/project1.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/qgwil/cs236/project1/CommentAutomaton.cpp" "C:/Users/qgwil/cs236/project1/cmake-build-debug/CMakeFiles/project1.dir/CommentAutomaton.cpp.obj"
+  "C:/Users/qgwil/cs236/project1/ExactMatchAutomaton.cpp" "C:/Users/qgwil/cs236/project1/cmake-build-debug/CMakeFiles/project1.dir/ExactMatchAutomaton.cpp.obj"
+  "C:/Users/qgwil/cs236/project1/IDAutomaton.cpp" "C:/Users/qgwil/cs236/project1/cmake-build-debug/CMakeFiles/project1.dir/IDAutomaton.cpp.obj"
+  "C:/Users/qgwil/cs236/project1/Lexer.cpp" "C:/Users/qgwil/cs236/project1/cmake-build-debug/CMakeFiles/project1.dir/Lexer.cpp.obj"
+  "C:/Users/qgwil/cs236/project1/StringAutomaton.cpp" "C:/Users/qgwil/cs236/project1/cmake-build-debug/CMakeFiles/project1.dir/StringAutomaton.cpp.obj"
+  "C:/Users/qgwil/cs236/project1/Token.cpp" "C:/Users/qgwil/cs236/project1/cmake-build-debug/CMakeFiles/project1.dir/Token.cpp.obj"
+  "C:/Users/qgwil/cs236/project1/main.cpp" "C:/Users/qgwil/cs236/project1/cmake-build-debug/CMakeFiles/project1.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/project1.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project1.dir/CommentAutomaton.cpp.obj"
+  "CMakeFiles/project1.dir/ExactMatchAutomaton.cpp.obj"
+  "CMakeFiles/project1.dir/IDAutomaton.cpp.obj"
+  "CMakeFiles/project1.dir/Lexer.cpp.obj"
+  "CMakeFiles/project1.dir/StringAutomaton.cpp.obj"
+  "CMakeFiles/project1.dir/Token.cpp.obj"
+  "CMakeFiles/project1.dir/main.cpp.obj"
+  "libproject1.dll.a"
+  "project1.exe"
+  "project1.exe.manifest"
+  "project1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
